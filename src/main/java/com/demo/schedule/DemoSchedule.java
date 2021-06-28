427bbb4aebbe6a82127b409934ec266f503d07be
labradorGitHubCanine <123349527@qq.com>
2021-06-28 17:54:33 +0800

    0

--- src/main/java/com/demo/schedule/DemoSchedule.java
+++ src/main/java/com/demo/schedule/DemoSchedule.java
@@ -4,20 +4,23 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Async
 @Component
 public class DemoSchedule {
 
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     @Scheduled(fixedDelay = 1000 * 60)
     public void schedule01() {
-        System.out.println(new Timestamp(System.currentTimeMillis()) + "\t每60秒执行一次");
+        System.out.println(LocalDateTime.now().format(FORMATTER) + "\t每60秒执行一次");
     }
 
     @Scheduled(cron = "0 0 1 * * ?")
     public void schedule02() {
-        System.out.println(new Timestamp(System.currentTimeMillis()) + "\t每天1点执行一次");
+        System.out.println(LocalDateTime.now().format(FORMATTER) + "\t每天1点执行一次");
     }
 
 }
